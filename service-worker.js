5794be961453f36860d9b2866b6160f2ccb06e5a
alex <boomscrip@gmail.com>
2019-02-27 23:09:35 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/creatora/precache-manifest.c6c5b87472c5fb8fa6d85db70ce76fb9.js"
+  "/creatora/precache-manifest.5a377f1a7c594e235f3e30b7fdc7264d.js"
 );
 
 workbox.clientsClaim();
